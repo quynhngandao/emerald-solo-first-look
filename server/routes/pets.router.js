690e341bbc5b13a2d9b94b8a22414e666395f96e
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-25 14:53:29 -0500

    done

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
+const {rejectUnauthenticated } = require('../modules/authentication-middleware'
+)
 
 // This route *should* return the logged in users pets
-router.get('/', (req, res) => {
+// rejectUnauthenticated middleware used as a protection, ensuring only authenticated users can access this route
+// req.isAuthenticated() check if user is authenticated 
+router.get('/', rejectUnauthenticated , (req, res) => {
     console.log('/pet GET route');
+    // is user logged in? 
     console.log('is authenticated?', req.isAuthenticated());
+    // who is the user logged in?
     console.log('user', req.user);
-    let queryText = `SELECT * FROM "pets"`;
-    pool.query(queryText).then((result) => {
+
+    // if (req.isAuthenticated()) => { 
+        let queryText = `SELECT * FROM "pets" WHERE "user_id" = $1`;
+    pool.query(queryText, [req.user.id]).then((result) => {
         res.send(result.rows);
     }).catch((error) => {
         console.log(error);
         res.sendStatus(500);
-    });
+    })  
+    // } else {
+     // res.sendStatus(403)
 });
 
 // This route *should* add a pet for the logged in user
